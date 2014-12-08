3824af61f90cc4ca036719a280adbc08cd53e99a
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-12-07 21:59:16 -0800

    Initial Commit

---
+++ CSE167 Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE167 Final Project
+//
+//  Created by Ruiqing Qiu on 12/7/14.
+//  Copyright (c) 2014 Ruiqing Qiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
