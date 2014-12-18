7ee59738f054e37bcbec74ad7f6f7834365e0f22
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-12-17 23:15:29 -0800

    merged.

--- Terrain.cpp
+++ Terrain.cpp
@@ -28,8 +28,8 @@ Terrain1::Terrain1( int size ) {
     grassTex = SOIL_load_OGL_texture
     (
      //strcat(pre,name),
-     "/Users/Ennuma/Desktop/CSE167_Final_Project/grass.jpg",
-     //"/Users/ruiqingqiu/Desktop/Qiu_Code/CSE167/CSE167 Final Project/grass.jpg",
+     //"/Users/Ennuma/Desktop/CSE167_Final_Project/grass.jpg",
+     "/Users/ruiqingqiu/Desktop/Qiu_Code/CSE167/CSE167 Final Project/grass.jpg",
      //"/Users/margaretwm3/Dropbox/CSE167_Final_Project/grass.jpg",
      SOIL_LOAD_AUTO,
      SOIL_CREATE_NEW_ID,
